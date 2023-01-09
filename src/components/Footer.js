c404efc332dfebefde4a7fa852a260caee07a543
aguss rodriguez <agussrg80@gmail.com>
2023-01-09 19:51:52 -0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ import React from "react";
 const Footer = () => {
 	return (
 		<div className="text-center text-xs md:text-sm p-2 md:p-4 text-slate-500">
-			<p>
-				Este contenido no ha sido creado ni aprobado por Google. Notificar uso
-				inadecuado - Términos del Servicio - Política de Privacidad
-			</p>
 			<h4>Challenge GreyDive 🥰</h4>
+			<span>by </span>
+			<a target="blank" href="https://nahuro.github.io/Portfolio/">
+				Aguss 💻
+			</a>
 		</div>
 	);
 };
