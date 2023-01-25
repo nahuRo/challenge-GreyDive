dc25cd3b4e0797b20a6b06305a73b62480f99aeb
aguss rodriguez <agussrg80@gmail.com>
2023-01-25 18:53:54 -0300

    fix name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import React from "react";
 const Footer = () => {
 	return (
 		<div className="text-center text-xs md:text-sm p-2 md:p-4 text-slate-500">
-			<h4>Challenge GreyDive 🥰</h4>
-			<span>by </span>
+			<span className="mr-1">Challenge by</span>
 			<a target="blank" href="https://nahuro.github.io/Portfolio/">
 				Aguss 💻
 			</a>
